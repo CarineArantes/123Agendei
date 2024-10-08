7df770af3d19147765714d8ca66a30752b96595a
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-10-08 17:37:17 -0300

    clear__

--- src/components/Modal/types.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type ModalProps = {
-    visible: boolean;
-    transparent: boolean;
-    animationType: 'none' | 'slide' | 'fade' | undefined;
-    onRequestClose: () => void;
-    children?: React.ReactNode;
-}
