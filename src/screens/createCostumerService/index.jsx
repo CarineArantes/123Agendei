752755657e63eb57c165456bd938597801a7a0bc
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-10-12 11:53:38 -0300

    Update index.jsx

--- src/screens/createCostumerService/index.jsx
+++ src/screens/createCostumerService/index.jsx
@@ -2,11 +2,10 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from 'react-native'
 
 export function CreateCostumerService() {
+
     return (
         <SafeAreaView className=" flex-1">
-            <Text>
-                CreateCostumerService
-            </Text>
+            <Text> CreateCostumerService </Text>
         </SafeAreaView>
     )
 }
